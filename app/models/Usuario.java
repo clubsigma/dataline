855a002cc6b9845a50b5f8af361b62f46ce84032
ljimenezUTC <luisjimenez.r8@gmail.com>
2015-07-31 04:25:11 +0200

    Horario.java
    Incremento de campos en la clase horario

--- app/models/Usuario.java
+++ app/models/Usuario.java
@@ -14,13 +14,15 @@ import play.db.jpa.Model;
 
 @Entity
 public class Usuario extends Model {
+	 public String Cedula;
 	 public String nombre;
 	 public String apellido;
 	 public  String password;
 	 public  String email;
     //
-	public Usuario(String nombre, String apellido, String password, String email) {
+	public Usuario(String cedula, String nombre, String apellido, String password, String email) {
 		super();
+		this.Cedula = cedula;
 		this.nombre = nombre;
 		this.apellido = apellido;
 		this.password = password;
