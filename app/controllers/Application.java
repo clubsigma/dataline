b7da57bbee1b96154ab190c5dacab75306f3e220
acarreraUTC <anderson.carrera45@gmail.com>
2015-08-05 15:41:21 -0500

    Mostrar Datos de Carrera
    Tabla Carrera

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -33,6 +33,8 @@ public class Application extends Controller {
         render();
     }
     public static void distributivo() {
+    	List<Carrera> carrera = Carrera.findAll();
+		render(carrera);
         render();
     }
     
